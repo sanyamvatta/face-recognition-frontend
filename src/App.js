885e96de3571e0cd2cb91037600d0610c7d08dea
Sanyam vatta <sanyamvatta4@gmail.com>
2023-03-03 08:03:28 +0530

    change api routes

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
 
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://face-recognition-backend-aqm0.onrender.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -78,7 +78,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://face-recognition-backend-aqm0.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
